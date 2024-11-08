85db365ce2bbae1522dfc6254d6cecb4679414e6
guihlherme0 <fransciscoguilherme18@gmail.com>
2024-11-08 20:29:45 -0300

    primeiro commit

---
+++ usar_bd.py
@@ -0,0 +1,13 @@
+import DAO
+
+# saida = DAO.inserir_usuario('guilherme', 'guilherme@gmail.com', '200518')
+#saida = DAO.inserir_usuario('Joao', 'joao@email.com', 'senhaJoao')
+#saida = DAO.buscar_usuario('guilherme')
+#saida = DAO.listar_usuarios()
+
+#saida = DAO.atualizar_usuario(2, 'Joao alterar', 'joaoAlterar@email.com')
+
+#saida = DAO.deletar_usuario(2)
+
+#saida = DAO.listar_usuarios()
+#print(saida)
